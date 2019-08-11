bc20f3aa1c6675b496c76815b37ae44c634466f6
Ross Young <xenon87@gmail.com>
2019-08-11 20:25:14 +0100

    * Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-JustEatHygiene
+# JustEat - Visible Hygiene Ratings
+
+
+Displays hygiene ratings from the Food Standards Agency for restaurants without extra clicks.
+Lets you hide or remove restaurants that don't meet your rating.
+
+This script is for use on the [Just Eat](https://just-eat.co.uk) website using the [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) chrome plugin.
+
+## Install
+1. Install the [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) chrome plugin.
+2. Click the Tampermonkey icon in chrome and select `"Create a new script..."`
+3. Copy and paste the contents of `build/main.js` file into the text area.
+4. In the editor select `"File"` then `"Save"`.
+5. Done! Now visit [Just Eat](https://just-eat.co.uk) to se it in action.
+
+## Options
+
+**Cache Time**
+
+The amount of time in seconds, to cache the results from the Food Standards Agency. The default is 7 days.
+
+```javascript 1.8
+let CACHE_TIME = 60 * 60 * 24 * 7; // Defaults to 7 days
+```
+---
+**Bad Restaurant Action**
+
+The "Bad restaurant Action" option is the action to take if the minimum rating threshold (see below) is not reached.
+
+There are currently *4* options available.
+
+Value | Action
+------|--------
+`BadResultAction.NoAction`| Does nothing with matched results.
+`BadResultAction.Delete` | Removes the matched results from the page.
+`BadResultAction.FadeOnly`| Fades the matched items in place.
+`BadResultAction.MoveAndFade` | Moves the matched items to the bottom of the list and fades them.
+ 
+ ```javascript 1.8
+let BAD_RESTAURANT_ACTION = BadResultAction.MoveAndFade; // Defaults to BadResultAction.MoveAndFade
+```
+---
+**Minimum Rating Threshold**
+
+The minimum rating threshold is the lowest rating a restaurant can have before an action is taken. The default value is 4. 
+
+```javascript 1.8
+let MIN_RATING_THRESHOLD = 4; // Defaults to 4
+```
---
+++ scripts/main.ts
@@ -0,0 +1,125 @@
+// ==UserScript==
+// @name         JustEat - Visible Hygiene Ratings
+// @version      1.0
+// @description  Displays hygiene ratings for restaurants without extra clicks. Lets you hide or remove restaurants that don't meet your rating.
+// @author       Ross Young
+// @match        https://www.just-eat.co.uk/area/*
+// @grant        none
+// ==/UserScript==
+(function () {
+    'use strict';
+
+    enum BadResultAction {
+        NoAction,
+        Delete,
+        MoveAndFade,
+        FadeOnly
+    }
+
+    /*********************************************************
+     * [CONFIGURATION] Cache Time
+     * The amount of time (in seconds) to cache the results from
+     * the Food Standards Agency.
+     *
+     * Defaults to 7 days.
+     *
+     * Default: 60 * 60 * 24 * 7
+     *********************************************************/
+    let CACHE_TIME = 60 * 60 * 24 * 7;
+
+    /*********************************************************
+     * [CONFIGURATION] Minimum Rating Threshold
+     * The minimum number of stars (from the hygiene
+     * rating website) required before an action is
+     * taken.
+     *
+     * Default: 4
+     *********************************************************/
+    let MIN_RATING_THRESHOLD = 4;
+
+    /*********************************************************
+     * [CONFIGURATION] Bad Restaurant Action
+     * Which actions to perform if minimum rating
+     * has not been reached. Available options are:
+     *
+     * - BadResultAction.Delete       | Removes item from list.
+     * - BadResultAction.MoveAndFade  | Moves the item to the bottom of the list and fades it out.
+     * - BadResultAction.FadeOnly     | Fades out the item in place.
+     * - BadResultAction.NoAction     | Do nothing.
+     *
+     * Default: BadResultAction.MoveAndFade
+     *********************************************************/
+    let BAD_RESTAURANT_ACTION: BadResultAction = BadResultAction.MoveAndFade;
+
+    interface HygieneRating {
+        imageUrl: string,
+        alternativeText: string,
+        moreInfoUrl: string,
+        status: string,
+        lastInspectionDate: string,
+        rating: number,
+        localCacheDate: number
+    }
+
+    class RestaurantManager {
+
+        url: string = "https://hygieneratingscdn.je-apis.com/api/uk/restaurants/";
+        cacheTime: number;
+
+        constructor(cacheTime: number) {
+            this.cacheTime = cacheTime;
+        }
+
+        getRestaurant(key: string): HygieneRating {
+            let data: HygieneRating | null = JSON.parse(window.localStorage.getItem(key));
+            if (null === data || (((Date.now() - data.localCacheDate) / 1000) > this.cacheTime)) {
+                return this.fetchData(key);
+            }
+            return data;
+        }
+
+        private fetchData(key: string): HygieneRating {
+            let url = this.url + key;
+            let request = new XMLHttpRequest();
+            request.open('GET', url, false);
+            request.send(null);
+            let data = <HygieneRating>JSON.parse(request.responseText);
+            data.localCacheDate = Date.now();
+            window.localStorage.setItem(key, JSON.stringify(data));
+            return data;
+        }
+    }
+
+    const restaurants: NodeListOf<HTMLElement> = document.querySelectorAll('section[data-restaurant-id]');
+    let manager = new RestaurantManager(CACHE_TIME);
+
+    restaurants.forEach(function (element: HTMLElement) {
+        let id = element.getAttribute('data-restaurant-id');
+        let data = manager.getRestaurant(id);
+        let img = new Image();
+        img.src = data.imageUrl;
+        img.width = 100;
+
+        img.style.width = "100px";
+        img.style.position = "absolute";
+        img.style.right = "23px";
+        img.style.top = "25%";
+        element.append(img);
+
+        if (data.rating < MIN_RATING_THRESHOLD) {
+            switch (BAD_RESTAURANT_ACTION) {
+                case BadResultAction.Delete:
+                    element.remove();
+                    break;
+                case BadResultAction.MoveAndFade:
+                    element.style.opacity = "0.2";
+                    element.parentElement.appendChild(element);
+                    break;
+                case BadResultAction.FadeOnly:
+                    element.style.opacity = "0.2";
+                    break;
+            }
+        }
+
+    });
+})();
